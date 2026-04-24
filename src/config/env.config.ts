ade915e2534e5fb05129c6278ffae55371cef55f
Denilson Santos <denilsonsantos.dev@gmail.com>
2026-04-24 15:49:53 -0300

    feature: integração de validação de variáveis de ambiente durante inicialização com Joi

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -1,6 +1,19 @@
 import { ConfigModule } from '@nestjs/config';
 
+import * as Joi from 'joi';
+
 export const envConfig = ConfigModule.forRoot({
   isGlobal: true,
   envFilePath: [`.env.${process.env.NODE_ENV}`, '.env'],
+
+  validationSchema: Joi.object({
+    NODE_ENV: Joi.string().valid('development', 'production', 'test'),
+
+    // Adicione novas validações de variáveis aqui embaixo para verificá-las na inicializaçõo do projeto
+  }),
+
+  validationOptions: {
+    allowUnknown: true, // permite outras env vars do sistema
+    abortEarly: false, // mostra todos os erros juntos
+  },
 });
